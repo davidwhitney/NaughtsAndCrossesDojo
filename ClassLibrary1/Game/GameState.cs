4bb46c267b4a16c9276a8f7000ff5718019f3457
David Whitney <david@davidwhitney.co.uk>
2015-12-08 12:08:48 +0000

    Removed unused

--- ClassLibrary1/Game/GameState.cs
+++
@@ -1,9 +0,0 @@
-namespace ExesAndOhhs.Game
-{
-    public class GameState
-    {
-        public string Drawn { get; set; }
-        public bool GameWon { get; set; }
-        public char Winner { get; set; }
-    }
-}
